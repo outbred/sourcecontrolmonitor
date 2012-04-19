84003bfe9ae03f04bf8b6ccd38facc5c15fa62f1
Brent Bulla <outbred@gmail.com>
2012-04-18 22:28:11 -0600

    works on my other box

--- SourceControlMonitor/App.xaml.cs
+++ SourceControlMonitor/App.xaml.cs
@@ -27,6 +27,10 @@ namespace SourceControlMonitor
 			}
 
 			Trace.TraceError("Unhandled exception in this simple app. Did someone forget to eat their Wheaties?\n\n{0}", ex.Message);
+			if(ex.InnerException != null)
+			{
+				Trace.TraceError("Inner exception:\n", ex.InnerException.Message);
+			}
 			//ExceptionPolicy.HandleException(ex, "Default Policy");
 			MessageBox.Show(string.Format("An unhandled exception has occurred: {0}\n{1}\n\n{2}\n{3}", ex.Message, ex.StackTrace, ex.InnerException != null ? ex.InnerException.Message : null, ex.InnerException != null ? ex.InnerException.StackTrace : null), "Error",
 							MessageBoxButton.OK, MessageBoxImage.Error);
